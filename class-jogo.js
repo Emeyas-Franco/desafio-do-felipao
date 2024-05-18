d32ff9ad6bbfdefb344e184efc6d0544dda878a4
Emeyas Franco <emeyasdev@gmail.com>
2024-05-18 10:24:08 -0300

    Desafio 3 - DIO
    Desafio 3 proposto pelo Felipão na formação Lógica de Programação

---
+++ class-jogo.js
@@ -0,0 +1,33 @@
+class hero{
+    constructor(nameHero, ageHero, typeHero){
+        this.nameHero = nameHero
+        this.ageHero = ageHero
+        this.typeHero = typeHero
+    }
+    atacar(){
+        let attack
+        switch (this.typeHero){
+            case "mage":
+                attack = "magia";
+                break
+            case "warrior":
+                attack = "espada";
+                break
+            case "monk":
+                attack = "artes marciais";
+                break
+            case "ninja":
+                attack = "shuriken";
+        }
+        console. log(`O ${this.typeHero} ${this.nameHero} atacou usando ${attack}`)
+    }
+}
+        
+let mage = new hero("Emeyas", 38, "mage")
+let warrior = new hero("Arthur", 24, "warrior")
+let monk = new hero("Tenzin", 32, "Monge")
+let ninja = new hero(`Akarai`, 27, `Ninja`)
+
+monk.atacar()
+
+
